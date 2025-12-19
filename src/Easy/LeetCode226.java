61c2a27ca0cbbc84b57b9bf8a72b2765af2fdfa3
mehulkunwar <mehulkunwar444@gmail.com>
2025-12-19 14:23:25 +0530

    Added LeetCode Questions - 226

---
+++ src/Easy/LeetCode226.java
@@ -0,0 +1,17 @@
+package Easy;
+
+import Utility.TreeNode;
+
+public class LeetCode226 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
